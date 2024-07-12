3608bb1c26c5d5e9dcaab1c0d45dcdc094d4df74
kushagraag <kushjanhvi@gmail.com>
2024-07-13 01:18:31 +0530

    logo access locally

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,23 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const Logo: React.FC = () => {
   return (
     <div className="logo">
-      <a
-        href="https://thementalchunks.com/"
-        className="custom-logo-link"
-        rel="home"
-      >
-        <img
+      <Link href="https://thementalchunks.com/">
+        <Image
           width="100"
           height="100"
-          src="https://thementalchunks.com/wp-content/uploads/2024/06/Orange-Blue-Brain-Digital-World-Logo.png"
+          src="/assets/Orange-Blue-Brain-Digital-World-Logo.png"
           className="custom-logo"
           alt="The Mental Chunks"
-          itemprop="logo"
           decoding="async"
         />
-      </a>
+      </Link>
     </div>
   );
 };
