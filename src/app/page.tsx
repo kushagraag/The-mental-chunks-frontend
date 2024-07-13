235aaa48d31de236055e49d0e6b1a557e81b4a74
kushagraag <kushjanhvi@gmail.com>
2024-07-14 01:48:35 +0530

    footer done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import HomepageContent from "@/components/HomepageContent";
-import Header from "@/components/Header";
-import { Footer, Footer2 } from "@/components/Footer";
+import Footer from "@/components/Footer";
 import "../styles/globals.css";
 
 export default function Home() {
   return (
     <div className="bg-[#FFE6E2]">
-      {/* <Header /> */}
       <HomepageContent />
-      {/* <Footer /> */}
-      <Footer2 />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,151 +1,66 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import Logo from "./Logo";
-
-const Footer2: React.FC = () => {
-  return (
-    <footer className="footer-class">
-      {/* orange bg */}
-      <div>
-        <Logo />
-      </div>
-      <div>
-        <p>Mental Health for everyone</p>
-      </div>
-      <div>
-        <ul>
-          <li>Facebook</li>
-          <li>Instagram</li>
-          <li>Twitter</li>
-        </ul>
-      </div>
-      {/* black bg */}
-      <div>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/privacyPolicy">Privacy Policy</Link>
-          </li>
-          <li>
-            <Link href="/aboutUs">About Us</Link>
-          </li>
-          <li>
-            <Link href="/articles/blogs">Articles</Link>
-          </li>
-          <li>
-            <Link href="/contactUs">Contact Us</Link>
-          </li>
-          <li>
-            <Link href="/termsAndConditions">Terms and Conditions</Link>
-          </li>
-        </ul>
-      </div>
-
-      <div>
-        <span>Copyright &copy; 2024 </span>
-        The Mental Chunks
-      </div>
-    </footer>
-  );
-};
 
 const Footer: React.FC = () => {
   return (
-    <footer
-      id="footer"
-      className="clearfix site-footer"
-      role="contentinfo"
-      itemscope
-      itemtype="https://schema.org/WPFooter"
-    >
-      <div className="footer-bar grid col-940">
-        <div className="content-outer container">
-          <div className="row">
-            <nav
-              id="footer-site-navigation"
-              className="footer-navigation"
-              role="navigation"
-              aria-label="Footer Menu"
-            >
-              <h2 className="screen-reader-text">Footer Menu</h2>
-              <ul id="footer-menu" className="menu">
-                <li
-                  id="menu-item-1473"
-                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-9 current_page_item menu-item-1473"
-                >
-                  <a href="https://thementalchunks.com/" aria-current="page">
-                    Home
-                  </a>
-                </li>
-                <li
-                  id="menu-item-1474"
-                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-privacy-policy menu-item-1474"
-                >
-                  <a
-                    rel="privacy-policy"
-                    href="https://thementalchunks.com/privacy-policy/"
-                  >
-                    Privacy Policy
-                  </a>
-                </li>
-                <li
-                  id="menu-item-1475"
-                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1475"
-                >
-                  <a href="https://thementalchunks.com/about-us/">About Us</a>
-                </li>
-                <li
-                  id="menu-item-1476"
-                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1476"
-                >
-                  <a
-                    href="https://thementalchunks.com/articles/"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Articles
-                  </a>
-                </li>
-                <li
-                  id="menu-item-1477"
-                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1477"
-                >
-                  <a href="https://thementalchunks.com/contact-us/">
-                    Contact Us
-                  </a>
-                </li>
-                <li
-                  id="menu-item-1478"
-                  className="menu-item menu-item-type-post_type menu-item-object-page menu-item-1478"
-                >
-                  <a href="https://thementalchunks.com/terms-and-conditions/">
-                    Terms and Conditions
-                  </a>
-                </li>
-              </ul>
-            </nav>
-            <div className="footer-layouts copyright">
-              <div className="footer-layouts copyright">
-                <span className="copyright_icon_and_year">
-                  Copyright &copy; 2024{" "}
-                </span>
-                The Mental Chunks <span>| Powered by </span>
-                <a
-                  href="
-		https://cyberchimps.com/			"
-                  target="_blank"
-                >
-                  Responsive Theme{" "}
-                </a>
-              </div>
-            </div>
-          </div>
+    <footer className="footer-class">
+      <div className="bg-[#F26044]  p-8">
+        <div className="mt-20 grid place-content-center">
+          <Logo width={200} height={200} />
+        </div>
+        <div className="mt-12 grid place-content-center">
+          <p className="text-[32px] text-white font-bold">
+            Mental Health for everyone
+          </p>
+        </div>
+        <div className="flex justify-center my-16 space-x-24">
+          <Link href="#">
+            <Image
+              src="/assets/facebook.png"
+              width="48"
+              height="48"
+              alt="facebook"
+              className="m-4"
+            />
+          </Link>
+          <Link href="#">
+            <Image
+              src="/assets/instagram.png"
+              width="48"
+              height="48"
+              alt="instagram"
+              className="m-4"
+            />{" "}
+          </Link>
+          <Link href="#">
+            <Image
+              src="/assets/twitter.png"
+              width="48"
+              height="48"
+              alt="twitter"
+              className="m-4"
+            />{" "}
+          </Link>
+        </div>
+      </div>
+      {/* black bg */}
+      <div className="bg-black p-8 text-white">
+        <div className="flex justify-center my-16 space-x-12 text-[24px]">
+          <Link href="/">Home</Link>
+          <Link href="/privacyPolicy">Privacy Policy</Link>
+          <Link href="/aboutUs">About Us</Link>
+          <Link href="/articles/blogs">Articles</Link>
+          <Link href="/contactUs">Contact Us</Link>
+          <Link href="/termsAndConditions">Terms and Conditions</Link>
+        </div>
+        <div className="flex justify-center text-[16px]">
+          Copyright &copy; 2024 The Mental Chunks
         </div>
       </div>
     </footer>
   );
 };
 
-export { Footer, Footer2 };
+export default Footer;
