612801ee36c60d6404a22fed422de65a2a939b09
kushagraag <kushjanhvi@gmail.com>
2024-07-14 01:48:55 +0530

    make logo resizable

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,13 +2,18 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 
-const Logo: React.FC = () => {
+interface LogoProps {
+  width?: number;
+  height?: number;
+}
+
+const Logo: React.FC<LogoProps> = ({ width = 100, height = 100 }) => {
   return (
     <div className="logo">
       <Link href="https://thementalchunks.com/">
         <Image
-          width="100"
-          height="100"
+          width={width}
+          height={height}
           src="/assets/Orange-Blue-Brain-Digital-World-Logo.png"
           className="custom-logo"
           alt="The Mental Chunks"
