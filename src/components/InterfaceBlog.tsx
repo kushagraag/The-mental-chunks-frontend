26136e81948fdfe93a127278bc8cf3b62c42d2ca
kushagraag <kushjanhvi@gmail.com>
2024-07-15 04:09:30 +0530

    added interface for blog

---
+++ src/components/InterfaceBlog.tsx
@@ -0,0 +1,10 @@
+export default interface Blog {
+  id: string;
+  heading: string;
+  sub_heading: string;
+  date: string;
+  display_picture: string;
+  content: string;
+  comments: number;
+  likes: number;
+}
