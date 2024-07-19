24c00c4c78663a0166718cbb11b3f6c907e07f2f
kushagraag <kushjanhvi@gmail.com>
2024-07-19 19:53:13 +0530

    responsive, material ui header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,87 +1,135 @@
-import Logo from "./Logo";
+"use client";
+
 import React from "react";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Logo from "./Logo";
 import Link from "next/link";
 
-const Header: React.FC = () => {
+function NavList() {
   return (
-    <>
-      <header className=" py-6 ">
-        <div className="container flex justify-between items-center mx-auto px-36">
-          <div className="flex items-center">
+    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-4">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <Link href="/">
+          <p className="text-[20px] hover:text-[#F26044]">Home</p>
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <Link href="/aboutUs">
+          <p className="text-[20px] hover:text-[#F26044]">About Us</p>
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <Link
+          href={{
+            pathname: "/articles/blogs",
+            query: { page: 1, limit: 5 },
+          }}
+        >
+          <p className="text-[20px] hover:text-[#F26044] ">Blogs</p>
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <Link href="/trending">
+          <p className="text-[20px] hover:text-[#F26044]">Trending Blogs</p>
+        </Link>
+      </Typography>
+
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <Link href="/quotes">
+          <p className="text-[20px] hover:text-[#F26044]">Quotes</p>
+        </Link>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <Link href="/contactUs">
+          <p className="text-[20px] hover:text-[#F26044]">Contact Us</p>
+        </Link>
+      </Typography>
+    </ul>
+  );
+}
+
+const Header: React.FC = () => {
+  const [openNav, setOpenNav] = React.useState(false);
+
+  const handleWindowResize = () =>
+    window.innerWidth >= 960 && setOpenNav(false);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+  return (
+    <div>
+      <Navbar className="mx-auto max-w-screen-xl px-6 py-3 my-4">
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="#"
+            variant="h6"
+            className="mr-4 cursor-pointer py-1.5"
+          >
             <Logo />
+          </Typography>
+          <div className="hidden lg:block">
+            <NavList />
           </div>
-          <nav>
-            <ul
-              className="flex space-x-8 text-black "
-              style={{ fontWeight: 400 }}
-            >
-              <li>
-                <Link href="/">
-                  <p className="text-[20px] hover:text-[#F26044]">Home</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/aboutUs">
-                  <p className="text-[20px] hover:text-[#F26044]">About Us</p>
-                </Link>
-              </li>
-              <li className="relative group">
-                <Link href="#" className=" flex items-center">
-                  <p className="text-[20px] hover:text-[#F26044]">Articles</p>
-                  <svg
-                    className="w-4 h-4 ml-1"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M19 9l-7 7-7-7"
-                    ></path>
-                  </svg>
-                </Link>
-                <ul className="absolute hidden group-hover:grid shadow-2xl mt-1">
-                  <li>
-                    <Link
-                      href={{
-                        pathname: "/articles/blogs",
-                        query: { page: 1, limit: 5 },
-                      }}
-                      className="block px-4 py-2 text-black hover:bg-[#F26044] hover:text-[#FFFFFF]"
-                    >
-                      <p className="text-[20px] hover:text-[#FFFFFF] ">Blogs</p>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/trending"
-                      className="block px-4 py-2 text-black hover:bg-[#F26044] hover:text-[#FFFFFF]"
-                    >
-                      <p className="text-[20px] hover:text-[#FFFFFF]">
-                        Trending Blogs
-                      </p>
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Link href="/quotes">
-                  <p className="text-[20px] hover:text-[#F26044]">Quotes</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contactUs">
-                  <p className="text-[20px] hover:text-[#F26044]">Contact Us</p>
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+            ) : (
+              <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+            )}
+          </IconButton>
         </div>
-      </header>
-    </>
+        <Collapse open={openNav}>
+          <NavList />
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
---
+++ src/components/Headerv1.tsx
@@ -0,0 +1,90 @@
+import Logo from "./Logo";
+import React from "react";
+import Link from "next/link";
+import { NavbarSimple } from "@/components/NavList";
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <NavbarSimple />
+      <header className=" py-6 ">
+        <div className="container flex justify-between items-center mx-auto px-36">
+          <div className="flex items-center">
+            <Logo />
+          </div>
+          <nav>
+            <ul
+              className="flex space-x-8 text-black "
+              style={{ fontWeight: 400 }}
+            >
+              <li>
+                <Link href="/">
+                  <p className="text-[20px] hover:text-[#F26044]">Home</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/aboutUs">
+                  <p className="text-[20px] hover:text-[#F26044]">About Us</p>
+                </Link>
+              </li>
+              <li className="relative group">
+                <Link href="#" className=" flex items-center">
+                  <p className="text-[20px] hover:text-[#F26044]">Articles</p>
+                  <svg
+                    className="w-4 h-4 ml-1"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    ></path>
+                  </svg>
+                </Link>
+                <ul className="absolute hidden group-hover:grid shadow-2xl mt-1">
+                  <li>
+                    <Link
+                      href={{
+                        pathname: "/articles/blogs",
+                        query: { page: 1, limit: 5 },
+                      }}
+                      className="block px-4 py-2 text-black hover:bg-[#F26044] hover:text-[#FFFFFF]"
+                    >
+                      <p className="text-[20px] hover:text-[#FFFFFF] ">Blogs</p>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/trending"
+                      className="block px-4 py-2 text-black hover:bg-[#F26044] hover:text-[#FFFFFF]"
+                    >
+                      <p className="text-[20px] hover:text-[#FFFFFF]">
+                        Trending Blogs
+                      </p>
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Link href="/quotes">
+                  <p className="text-[20px] hover:text-[#F26044]">Quotes</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contactUs">
+                  <p className="text-[20px] hover:text-[#F26044]">Contact Us</p>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
